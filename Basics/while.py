253c9f208900b27a8f5934e920a4957c80e7456f
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-11-25 00:38:47 +0100

    Learn while loop

---
+++ Basics/while.py
@@ -0,0 +1,7 @@
+number = 1
+
+while number <= 4:
+    print(number)
+    number += 1
+else:
+    print("End")
---
+++ Tasks/while.py
@@ -0,0 +1,10 @@
+number = 0
+suma = 0
+
+while number <= 100:
+    number += 1
+    suma += number
+else:
+    print("End")
+
+print(f"Sum: {suma}")
