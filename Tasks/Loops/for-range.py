29c7e53ef33e88caa4db5a1f5710907bc556828c
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-11-25 01:11:22 +0100

    Tasks with for in range loop

---
+++ Tasks/Loops/for-range.py
@@ -0,0 +1,21 @@
+sum = 0
+
+
+for num in range(0, 200):
+    sum += num
+
+print(f"Sum: {sum}")
+
+sum = 0
+
+for num in range(50, 100):
+    sum += num
+
+print(f"Sum: {sum}")
+
+sum = 0
+
+for num in range(0, 300, 3):
+    sum += num
+
+print(f"Sum: {sum}")
