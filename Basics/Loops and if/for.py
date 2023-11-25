8af0077ab8fb88ee77321efb86937294443f4770
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-11-25 01:06:27 +0100

    For loop in range

--- Basics/Loops and if/for.py
+++ Basics/Loops and if/for.py
@@ -2,3 +2,17 @@ listNumbers = [ 1, 2, 3, 4 ]
 
 for item in listNumbers:
     print(item)
+
+
+print()
+print("Range")
+print()
+
+#range
+for num in range(3):
+    print(num)
+
+print()
+
+for num in range(2, 8, 2):
+    print(num)
