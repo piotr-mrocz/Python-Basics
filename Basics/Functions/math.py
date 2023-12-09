a2d7fcb80c643aff12aeedb0425ab623a3f183b5
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-12-09 17:13:43 +0100

    Math functions

---
+++ Basics/Functions/math.py
@@ -0,0 +1,44 @@
+import math
+
+print("ABS")
+print(f"abs(5) = {abs(5)}")
+print(f"abs(-5) = {abs(-5)}")
+
+print()
+
+print("CEIL")
+print(f"math.ceil(1.22) = {math.ceil(1.22)}")
+print(f"math.ceil(7.93) = {math.ceil(7.93)}")
+
+print()
+
+print("FLOOR")
+print(f"math.floor(1.22) = {math.floor(1.22)}")
+print(f"math.floor(7.93) = {math.floor(7.93)}")
+
+print()
+
+print("MAX")
+print(f"max(1, 5, 9) = {max(1, 5, 9)}")
+print(f"max([2, 5, 8]) = {max([2, 5, 8])}")
+
+print()
+
+print("MIN")
+print(f"min(1, 5, 9) = {min(1, 5, 9)}")
+print(f"min([2, 5, 8]) = {min([2, 5, 8])}")
+
+print()
+
+print("POW")
+print(f"pow(2, 3) = {pow(2, 3)}")
+
+print()
+
+print("SQRT")
+print(f"math.sqrt(16) = {math.sqrt(16)}")
+
+print()
+
+print("ROUND")
+print(f"round(12.56464646, 3) = {round(12.56464646, 3)}")
